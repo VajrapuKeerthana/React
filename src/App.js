dbc1631d26fd0da46e275cf50ffb6b9a67620511
VajrapuKeerthana <2211cs010584@mallareddyuniversity.ac.in>
2025-02-05 12:56:54 +0530

    my react app

--- src/App.js
+++ src/App.js
@@ -16,26 +16,50 @@
 
 // export default App;
 
-import React from 'react'
-import Navbar from './components/Navbar'
-import Main from './components/Main'
-import Sidebar1 from './components/Sidebar1'
-import Sidebar2 from './components/Sidebar2'
-import Footbar from './components/Footbar'
+// import React from 'react'
+// import Navbar from './components/Navbar'
+// import Main from './components/Main'
+// import Sidebar1 from './components/Sidebar1'
+// import Sidebar2 from './components/Sidebar2'
+// import Footbar from './components/Footbar'
 
 
-const App = () => {
-  return (
-    <div className='App'>
-      <Navbar/>
-      <Main/>
-      <div className='sidebar'>
-      <Sidebar1/>
-      <Sidebar2/>
+// const App = () => {
+//   return (
+//     <div className='App'>
+//       <Navbar/>
+//       <Main/>
+//       <div className='sidebar'>
+//       <Sidebar1/>
+//       <Sidebar2/>
+//       </div>
+//       <Footbar/>
+
+//     </div>
+//   )
+// }
+// export default App
+import React, { Component } from 'react'
+import CBCPropEx from './propsex/CBCPropEx'
+import FBCPropEx from './propsex/FBCPropEx'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        {/* <CBCPropEx
+          username="keerthanavajrapu"
+          age={21}
+          hobbies={["singing" , "dancing"]}
+          address={{city:"nereducherla",area:"suryapet"}}
+          sendFun={function(){alert("Hi I am keerthanavajrapu")}}
+        /> */}
+        <FBCPropEx
+        username="Nikhil"
+        isMarried={true}
+        hobbies={["chatting","scolding","beating","bunking"]}
+        />
       </div>
-      <Footbar/>
-
-    </div>
-  )
+    )
+  }
 }
-export default App
---
+++ src/propsex/PropChildEx.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PropChildEx from './propsex/PropChildEx'
+import PropChildEx from './propsex/PropChildEx'
+const PropChildEx = () => {
+  return (
+    <div>PropChildEx</div>
+  )
+}
+
+export default PropChildEx
